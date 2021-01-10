36158397cf099bd6cea3f396a8dbfa9a0c05bafb
Hiroaki Iwata <tahitahi31@gmail.com>
2021-01-10 10:33:51 +0000

    Change settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')@^$r#iwfc1b!t*3ux+&juemdauemenzr@m#rb=kqb7$ktsb10'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com','.amazonaws.com']
 
